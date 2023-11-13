c3b1d054417ba003dc838baffdef86bbc46b063e
taeboranger <lightstone1@naver.com>
2023-11-14 06:46:19 +0900

    for test

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,5 @@
 const sum = require('./sum')
 
-test('1+2==3', () => {expect(sum(1, 2)).toBe(3)})
+//test('1+2==3', () => {expect(sum(1, 2)).toBe(3)})
 
-//test('1+2==4', () => {expect(sum(1, 2)).toBe(4)})
+test('1+2==4', () => {expect(sum(1, 2)).toBe(4)})
